6b83748b022c05ac29f63a26629e91149d0cb34f
Sagarxox <109059306+Sagarxox@users.noreply.github.com>
2023-03-08 22:13:45 +0530

    [Sagar] Add: Ability to create new Contact

---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the address Book System Problem");
+            AddressBookMain addressBookMain = new AddressBookMain();
+            addressBookMain.AddContact("sagar", "katahre", "yuuu", "pune", "MH", 441877, 01234456789, "abc@gmail.com");
+            addressBookMain.PrintList();
+            Console.ReadLine();
+        }
+    }
+}
